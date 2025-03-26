147439a314a74e4774931eae75dd07ef86cbe742
Benson Liao <skylock777@gmail.com>
2025-03-27 00:17:40 +0800

    Update omni-service-worker.js

--- omni-service-worker.js
+++ omni-service-worker.js
@@ -24,7 +24,7 @@ function postMessageToClients(message) {
   let clientData = {};
 
   self.addEventListener('install', () => {
-    console.log('install and activate 123');
+    console.log('install and activate 1234');
     const url = new URL(self.location);
     serviceWorkerMessageType = url.searchParams.get('serviceWorkerMessageType');
     self.skipWaiting();
@@ -77,7 +77,7 @@ function postMessageToClients(message) {
       postMessageToClients({ type: 'omnitag-error', payload: { error } });
     }
 
-    console.log('showNotification 123', title);
+    console.log('showNotification 1234', title);
     self.registration.showNotification(title, {
       body,
       icon,
