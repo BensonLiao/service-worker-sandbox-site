78a70a679e6d2a1b60104aa0fc9889a3271437f9
Benson Liao <skylock777@gmail.com>
2025-04-08 11:03:53 +0800

    Update omni-service-worker.js

--- omni-service-worker.js
+++ omni-service-worker.js
@@ -77,22 +77,22 @@ function postMessageToClients(message) {
       postMessageToClients({ type: 'omnitag-error', payload: { error } });
     }
 
-    console.log('showNotification in promise', title);
-    event.waitUntil(
-      self.registration.showNotification(title, {
-        body,
-        icon,
-        image: photo,
-        data: { url },
-      })
-    );
-    // console.log('showNotification', title);
-    // self.registration.showNotification(title, {
-    //   body,
-    //   icon,
-    //   image: photo,
-    //   data: { url },
-    // });
+    // console.log('showNotification in promise', title);
+    // event.waitUntil(
+    //   self.registration.showNotification(title, {
+    //     body,
+    //     icon,
+    //     image: photo,
+    //     data: { url },
+    //   })
+    // );
+    console.log('showNotification', title);
+    self.registration.showNotification(title, {
+      body,
+      icon,
+      image: photo,
+      data: { url },
+    });
   });
 
   self.addEventListener('error', event => {
