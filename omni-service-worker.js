971400663f72774f5de2ba8272d2609918278e82
Benson Liao <skylock777@gmail.com>
2025-04-09 20:00:28 +0800

    Update omni-service-worker.js

--- omni-service-worker.js
+++ omni-service-worker.js
@@ -39,7 +39,7 @@ function postMessageToClients(message) {
     }
   });
 
-  self.addEventListener('push', async event => {
+  self.addEventListener('push', event => {
     const {
       title,
       body,
@@ -95,35 +95,33 @@ function postMessageToClients(message) {
     // );
 
     console.log('delay showNotification and check if focused', title);
-    event.waitUntil(
-      new Promise(resolve => setTimeout(resolve, 4000)) // Delay for 4 seconds
-        .then(() =>
-          self.clients.matchAll().then(clientList => {
-            // Check if there's at least one focused client.
-            const focused = clientList.some(client => client.focused);
-            let additionalBody = '';
+    new Promise(resolve => setTimeout(resolve, 4000)) // Delay for 4 seconds
+      .then(() =>
+        self.clients.matchAll().then(clientList => {
+          // Check if there's at least one focused client.
+          const focused = clientList.some(client => client.focused);
+          let additionalBody = '';
 
-            if (focused) {
-              additionalBody = 'The page is focused.';
-            } else if (clientList.length > 0) {
-              additionalBody =
-                "You haven't closed the page, click here to focus it.";
-            } else {
-              additionalBody = 'The page is not focused.';
-            }
+          if (focused) {
+            additionalBody = 'The page is focused.';
+          } else if (clientList.length > 0) {
+            additionalBody =
+              "You haven't closed the page, click here to focus it.";
+          } else {
+            additionalBody = 'The page is not focused.';
+          }
 
-            console.log(additionalBody);
+          console.log(additionalBody);
 
-            // Show a notification with body depending on the state of the clients
-            return self.registration.showNotification(title, {
-              body: `${body} (${additionalBody})`,
-              icon,
-              image: photo,
-              data: { url },
-            });
-          })
-        )
-    );
+          // Show a notification with body depending on the state of the clients
+          return self.registration.showNotification(title, {
+            body: `${body} (${additionalBody})`,
+            icon,
+            image: photo,
+            data: { url },
+          });
+        })
+      );
   });
 })();
 
